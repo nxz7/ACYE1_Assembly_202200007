c1cf30027808d6fb48cdb64a3911d46cf80be00f
natalia <nataliawdyl@gmail.com>
2024-04-07 14:05:37 -0600

    LLL

--- practica.asm
+++ practica.asm
@@ -36,15 +36,25 @@ ImprimirBoard MACRO
         columna:
             MOV DL, matriz[SI]
             CMP filas[BX], 49 ;
-            JE SetPValueRow1
+            JE SetPValueRow1Aux
             CMP filas[BX], 50 ; fila 2
-            JE SetPValueRow2
+            JE SetPValueRow2Aux
             CMP filas[BX], 55 ;  7
-            JE SetPValueRow7
+            JE SetPValueRow7Aux
             CMP filas[BX], 56 ;8
-            JE SetPValueRow8
+            JE SetPValueRow8Aux
             INT 21h
-            JMP CheckEndOfRow
+            JMP CheckEndOfRowAux
+        SetPValueRow2Aux:
+        JMP SetPValueRow2
+        SetPValueRow7Aux:
+        JMP SetPValueRow7
+        SetPValueRow1Aux:
+        JMP SetPValueRow1
+        SetPValueRow8Aux:
+        JMP SetPValueRow8
+        CheckEndOfRowAux:
+        JMP CheckEndOfRow
         SetPValueRow1:
             MOV DL, 't' ; Column A
             CMP CL, 0
@@ -108,6 +118,10 @@ ImprimirBoard MACRO
         PrintColumn:
             INT 21h
             JMP CheckEndOfRow
+        columnaAux:
+        JMP columna
+        filaAux:
+        JMP fila
         CheckNextRow:
             MOV DL, matriz[SI]
             INT 21h
@@ -119,15 +133,13 @@ ImprimirBoard MACRO
             INC SI
 
             CMP CL, 8
-            JB columna
+            JB columnaAux
 
             MOV CL, 0
             INC BX
             CMP BX, 8
-            JB fila
+            JB filaAux
 ENDM
-
-
 
 RowMajorMatriz MACRO
     MOV AL, row
@@ -144,133 +156,8 @@ RowMajorMatriz MACRO
     MOV SI, AX
     MOV matriz[SI], 80
 ENDM
-
-CrearArchivo MACRO nombreArchivo, handler
-    LOCAL ManejarError, FinCrearArchivo
-    MOV AH, 3Ch ; REG LLAMADA
-    MOV CX, 00h ; AT
-    LEA DX, nombreArchivo ; nombre puntajes.txt
-    INT 21h
-
-    MOV handler, AX ; HANDLER
-    RCL BL, 1
-    AND BL, 1
-    CMP BL, 1
-    JE ManejarError
-    JMP FinCrearArchivo
-
-    ManejarError:
-        Print salto
-        Print errorCrearArchivo
-        getOpcion opcion
-    ;si si esta bien
-    FinCrearArchivo:
-ENDM
-
-AbrirArchivo MACRO nombreArchivo, handler
-    LOCAL ManejarError, FinAbrirArchivo
-    MOV BL, 0
-
-    MOV AH, 3Dh ; codigo de interrupcion
-    MOV AL, 00h ; modo de apertura del archivo, 0 -> Lectura | 1 -> Escritura | 2 -> Lectura/Escritura
-    LEA DX, nombreArchivo ; Nombre del archivo
-    INT 21h
-
-    MOV handler, AX ; Capturar el handler asignado al archivo (16 bits)
-    RCL BL, 1
-    CMP BL, 1
-    JE ManejarError
-    JMP FinAbrirArchivo
-
-    ManejarError:
-        Print salto
-        Print errorAbrirArchivo
-        getOpcion opcion
-
-    FinAbrirArchivo:
-ENDM
-
-CerrarArchivo MACRO handler
-    LOCAL ManejarError, FinCerrarArchivo
-
-    MOV AH, 3Eh ; Codigo de interrupcion
-    MOV BX, handler ; handler del archivo
-    INT 21h
-
-    RCL BL, 1
-    AND BL, 1
-    CMP BL, 1
-    JE ManejarError
-    JMP FinCerrarArchivo
-
-    ManejarError:
-        Print salto
-        Print errorCerrarArchivo
-        getOpcion opcion
-
-    FinCerrarArchivo:
-ENDM
-
-LeerArchivo MACRO buffer, handler
-    LOCAL ManejarError, FinLeerArchivo
-
-    MOV AH, 3Fh ; Codigo de interrupcion
-    MOV BX, handler ; handler del archivo
-    MOV CX, 300 ; Cantidad de bytes que se van a leer
-    LEA DX, buffer ; Posicion en memoria del buffer donde se almacenara el texto leido
-    INT 21h
-
-    MOV BL, 0
-    RCL BL, 1
-    CMP BL, 1
-    JE ManejarError
-    JMP FinLeerArchivo
-
-    ManejarError:
-        Print salto
-        Print errorLeerArchivo
-        getOpcion opcion
-    
-    FinLeerArchivo:
-ENDM
-
-EscribirArchivo MACRO cadena, handler
-    LOCAL ManejarError, FinEscribirArchivo
-
-    MOV AH, 40h ; Codigo de interrupcion
-    MOV BX, handler ; Handler de archivo
-    MOV CX, 120 ; Cantidad de bytes que se van a escribir
-    LEA DX, cadena1 ; Direccion de la cadena a escribir
-    INT 21h
-
-    RCL BL, 1 ; Capturar el bit de CF en el registro BL
-    AND BL, 1 ; Validar que en BL quede un 1 o 0
-    CMP BL, 1 ; Verificar si no hay codigo de error
-    JE ManejarError
-    JMP FinEscribirArchivo
-
-    ManejarError:
-        Print salto
-        Print errorEscribirArchivo
-        getOpcionOpcion opcion
-    
-    FinEscribirArchivo:
-ENDM
-
-PosicionarApuntador MACRO handler
-    MOV AH, 42h ; Codigo Interrupcion
-    MOV AL, 02h ; Modo de posicionamiento
-    MOV BX, handler ; handler archivo
-    MOV CX, 00h ; offset mas significativo
-    MOV DX, 00h ; offset menos significativo
-    INT 21h
-
-ENDM
-
 .MODEL small
-
 .STACK 64h
-
 .DATA
     salto db 10, 13, "$" ; \n
     mensajeInicio db " Universidad De San Carlos De Guatemala", 10, 13, " Facultad De Ingenieria", 10, 13, " Escuela de ciencias y sistemas", 10, 13," Arquitectura de computadores y ensambladores 1", 10, 13," SECCION A", 10, 13, "$"
@@ -278,7 +165,7 @@ ENDM
     mensajeSeparacion db "========================================", 10, 13, "$"
     mensajeTitulo db " ========CHESS - NATALIA MARIEL =========", 10, 13, "$"
     mensajeMenuContinuar db "Desea Continuar? (y/n): ", "$"
-    mensajeMenu db "l -->(1) Nuevo Juego                    l", 10, 13, "l -->(2) Puntajes                       l", 10, 13, "l -->(3) Reportes                       l", 10, 13, "l -->(4) Salir                          l", 10, 13, " --->>>>> Seleccione del menu: ", "$"
+    mensajeMenu db "l -->(1) Nuevo Juego      l", 10, 13, "l -->(2) Puntajes           l", 10, 13, "l -->(3) Reportes          l", 10, 13, "l -->(4) Salir              l", 10, 13, " --->>>>> Seleccione del menu: ", "$"
     msg1 db 10, 13, 10, 13, "Ingrese la fila: ", "$"
     msg2 db 10, 13, "Ingrese la columna: ", "$"
     matriz db 64 dup(32)
@@ -288,25 +175,49 @@ ENDM
     msgEFila db 10, 13, "La Fila Ingresada Es Incorrecta", "$"
     msgEColumna db 10, 13, "La Columna Ingresada Es Incorrecta", "$"
     opcionContinuar db 1 dup("$")
+    jugador_message db 10, 13, "natalia ========= $"
     opcion db 1 dup("$")
     row db 1 dup("$")
     col db 1 dup("$")
-    ;-----------------
-    handlerArchivo dw ? ; Handler o manejador del archivo de 16 bits
-    nombreArchivo db "puntaje.txt", 00h ; Nombre del archivo, DEBE TERMINAR EN CARACTER NULO
-    errorCrearArchivo db "Ocurrio Un Error Al Crear El Archivo", "$"
-    errorAbrirArchivo db "Ocurrio Un Error Al Abrir El Archivo", "$"
-    errorCerrarArchivo db "Ocurrio Un Error Al Cerrar Archivo", "$"
-    errorLeerArchivo db "Ocurrio Un Error Al Leer Archivo", "$"
-    errorEscribirArchivo db "Ocurrio Un Error Al Escribir Archivo", "$"
-    contentArchivo db "Este es un texto de prueba para escribir en los archivos"
-    archivoCreado db 10, 13, "El Archivo Se Creo Correctamente", "$"
-    buffer db 300 dup("$") ; Buffer para almacenar el contenido leido de un archivo
-
+    counter_units db 0   ; unidad
+    counter_tens db 0    ; decena
 .CODE
     MOV AX, @data
     MOV DS, AX
+    call Main
+; aumentar
+IncrementCounter PROC
+    mov al, [counter_units]
+    inc al
+    cmp al, 10      ; ver si 10
+    jne NoReset     ; si no salta el reset
+    mov al, 0       ; unidad 0
+    inc byte ptr [counter_tens] ; decena
+NoReset:
+    mov [counter_units], al
+    ret
+IncrementCounter ENDP
 
+; mostrar
+DisplayCounter PROC
+    ; Print "Jugador ========= "
+    mov ah, 09h       
+    mov dx, offset jugador_message 
+    int 21h           
+        
+    mov ah, 02h       ; 
+    mov al, [counter_tens] ; decena
+    add al, 30h       ; ASCII
+    mov dl, al
+    int 21h           ; 10
+    
+    mov al, [counter_units] ; unidad
+    add al, 30h       ; ASCII
+    mov dl, al
+    int 21h           ; unidad
+    ret
+DisplayCounter ENDP
+;--------------------------------------
     ; se imprime todo
     Main PROC
         LimpiarConsola
@@ -315,41 +226,43 @@ ENDM
         Print mensajeSeparacion
         Print mensajeDatos
         Print mensajeSeparacion
-
-
         Menu:
             Print mensajeTitulo
             Print mensajeMenu
             getOpcion opcion ; OPCION
             CMP opcion, 49 ; 1 - NUEVO JUEGI
             JE JUEGO
-            
             ;--------------------------------
             CMP opcion, 50 ; 2 - PUNTAJES
-            JE ImprimirPuntajes
-            
+            JE ImprimirPuntajesaux
             ;-------------------------------
             CMP opcion, 51 ; 3 - REPORTES
-            JE ImprimirReportes
-            
+            JE ImprimirReportesAux
             ;-----------------------------
             CMP opcion, 52 ; 4 - SALIR
-            JE Salir
+            JE SalirAux
             
+        ImprimirPuntajesaux:
+        JMP ImprimirPuntajes
+        ImprimirReportesAux:
+        JMP ImprimirReportes
+        SalirAux:
+        JMP Salir
         JUEGO:
             LimpiarConsola
             ImprimirBoard
+            call IncrementCounter
+            call DisplayCounter
             Print salto
             JMP PedirOpcionFila
             JMP Menu
 
         PedirOpcionFila:
             Print msg1
+            call IncrementCounter
             getOpcion row 
-            
             CMP row, 49
             JB ErrorFila
-            
             CMP row, 56
             JA ErrorFila
             JMP PedirOpcionColumna
@@ -361,6 +274,7 @@ ENDM
         
         PedirOpcionColumna:
             Print msg2
+            call IncrementCounter
             getOpcion col 
             
             CMP col, 65
@@ -383,8 +297,9 @@ ENDM
         ; si desea continuar
         Print mensajeMenuContinuar
         getOpcion opcionContinuar
-
-        CMP opcionContinuar, 121 ; 'y' 
+        
+        CMP opcionContinuar, 121 ; 'y'
+        
         JNE NoContinuar
 
         ; seguir
@@ -396,47 +311,13 @@ ENDM
 
         
         ImprimirPuntajes:
-
-
-        AuxSalir: 
-            JMP Salir
+        call IncrementCounter
+        call DisplayCounter
+        JMP Menu
 
         ImprimirReportes:
-            CrearArchivo nombreArchivo, handlerArchivo
-            CMP opcion, 13  
-            JE AuxSalir
-
-            PosicionarApuntador handler
-            EscribirArchivo contentArchivo, handlerArchivo
-            CMP opcion, 13  ; Verificar que no hay ningun error
-            JE AuxSalir4
-
-            CerrarArchivo handlerArchivo
-            CMP opcion, 13  ; Verificar que no hay ningun error
-            JE AuxSalir4
-            JMP ContinuarArchivos
-        
-        AuxSalir4:
-            JMP Salir
-
-        ContinuarArchivos:
-            Print archivoCreado
-            getOpcion opcion
-
-            AbrirArchivo nombreArchivo, handlerArchivo
-            CMP opcion, 13  ; Verificar que no hay ningun error
-            JE AuxSalir4
-
-            LeerArchivo buffer, handlerArchivo
-            CMP opcion, 13  ; Verificar que no hay ningun error
-            JE Salir
-
-            CerrarArchivo handlerArchivo
-            CMP opcion, 13 
-            JE Salir
 
         Salir:
-            Print buffer
             MOV AX, 4C00h 
             INT 21h
     Main ENDP
